d60d5f2df10092dc412311bd8fb929aa91411d4e
Julie Padilla <padilla410@gmail.com>
2021-10-20 14:07:49 -0400

    combine small data wrangling targets

--- _targets.R
+++ _targets.R
@@ -49,43 +49,24 @@ p1_targets_list <- list(
     nwis_site_info(fileout = "1_fetch/out/site_info.csv", site_data),
     format = "file"
   )
-
-  # make an `in_dir` style target to check the directory https://github.com/padilla410/ds-pipelines-targets-2/issues/3
-  
-  # tar_target(
-  #   site_data,
-  #   download_nwis_data(),
-  # ),
-  # tar_target(
-  #   site_info_csv,
-  #   nwis_site_info(fileout = "1_fetch/out/site_info.csv", site_data),
-  #   format = "file"
-  # )
 )
 
-# p2_targets_list <- list(
-#   tar_target(
-#     site_data_clean, 
-#     process_data(site_data)
-#   ),
-#   tar_target(
-#     site_data_annotated,
-#     annotate_data(site_data_clean, site_filename = site_info_csv)
-#   ),
-#   tar_target(
-#     site_data_styled,
-#     style_data(site_data_annotated)
-#   )
-# )
-# 
-# p3_targets_list <- list(
-#   tar_target(
-#     figure_1_png,
-#     plot_nwis_timeseries(fileout = "3_visualize/out/figure_1.png", site_data_styled),
-#     format = "file"
-#   )
-# )
+p2_targets_list <- list(
+  tar_target(
+    site_data_clean_csv,
+    process_data(nwis_data = site_data, nwis_site_data = site_info_csv),
+    format = "file"
+  )
+)
+
+p3_targets_list <- list(
+  tar_target(
+    figure_1_png,
+    plot_nwis_timeseries(filein = site_data_clean_csv, fileout = "3_visualize/out/figure_1.png"),
+    format = "file"
+  )
+)
 
 # Return the complete list of targets
-# c(p1_targets_list, p2_targets_list, p3_targets_list)
-c(p1_targets_list)
+c(p1_targets_list, p2_targets_list, p3_targets_list)
+
